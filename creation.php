0c6a888c5355eda6219e687d16590eeec806f432
juanpablocruz <pablo@cruzf.net>
2012-01-30 01:15:25 +0100

    Update creation.php

--- creation.php
+++ creation.php
@@ -3,11 +3,11 @@ $user="root";
 $host="localhost";
 $password="";
 $connection = mysql_connect($host,$user,$password)
-	or die ("couldn�t connect to server");
+	or die ("couldn't connect to server");
 $query = mysql_query("CREATE DATABASE qes");
 $database = "qes";
 $db = mysql_select_db($database,$connection)
-			or die ("Couldn�t select database");
+			or die ("Couldn't select database");
 $member = mysql_query("CREATE TABLE member(loginName varchar (20),password CHAR (255),createDate char(64),email char(64),phone CHAR (13), UserId int NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY(UserId))");
 $login = mysql_query("CREATE TABLE login(loginName char(20),loginTime datetime)");
