a9642d9408611f963e1bf43e7cf9e7883efdd16d
Juan Pablo <pablo@cruzf.net>
2012-01-30 01:08:29 +0100

    corrected bug in creation

--- creation.php
+++ creation.php
@@ -11,7 +11,7 @@ $db = mysql_select_db($database,$connection)
 $member = mysql_query("CREATE TABLE member(loginName varchar (20),password CHAR (255),createDate char(64),email char(64),phone CHAR (13), UserId int NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY(UserId))");
 $login = mysql_query("CREATE TABLE login(loginName char(20),loginTime datetime)");
-$images = mysql_query("CREATE TABLE images(Userid int(20),filename varchar(255),mime_type varchar(255),file_size int(11),file_data longblob)")
+$images = mysql_query("CREATE TABLE images(UserId int(20),filename varchar(255),mime_type varchar(255),file_size int(11),file_data longblob)")
 
 echo "Succes";
 ?>
